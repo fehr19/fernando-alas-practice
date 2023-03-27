80964fc3dd21b0343a0cc8ee906e5ebb3e8aef8c
Fernando Alas <fehr19@gmail.com>
2023-03-27 14:15:20 -0500

    Assignment 1 completed

---
+++ src/main/java/array/Main1.java
@@ -0,0 +1,11 @@
+package array;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        int[] myIntArray = {1, 2, 3};
+        for (int myIntArrayValue : myIntArray) {
+            System.out.println(myIntArrayValue);
+        }
+    }
+}
