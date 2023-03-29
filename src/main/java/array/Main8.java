6f17c7cb52e13c6056b9f1d34ee212c87efa8aa7
Fernando Alas <fehr19@gmail.com>
2023-03-29 00:08:15 -0500

    Finished Main 8 HW, with caveats. Need to work on printing unique results.

--- src/main/java/array/Main8.java
+++ src/main/java/array/Main8.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main8 {
     public static void main(String[] args) {
-        /*Read a line of text such as “I love Java.  You love Java. She loves Java.”
+        /*Read a line of text such as “I love Java. You love Java. She loves Java.”
         Split them into words. Count number of times each word (excluding dot .) occurs
         So the end result is something like following:
         "I" occurs once
@@ -20,22 +20,32 @@ public class Main8 {
         System.out.print("Please enter 2-3 short sentences: ");
         String sentencesInput = sc.nextLine();
 
-        String[] inputArray = sentencesInput.split("\\s+");
+        String[] inputArray = sentencesInput.replaceAll("\\p{Punct}", "").split("\\s+");
 
-        System.out.println(Arrays.toString(inputArray));
 
-        for (int i = 0; i < inputArray.length; i++) {
-            String tempString = inputArray[i];
-            for(int j = 0; j < inputArray.length - 1; j++);
+        //System.out.println(Arrays.toString(inputArray));
+
+        //initialize tempArray same length as inputArray
+        int a = inputArray.length;
+        String[] tempArray = new String[a];
+
+
+        for (int i = 0; i < a; i++) {
+
+            //reset counter before next comparison iteration
             int counter = 0;
-            if (tempString == inputArray[i + 1] ) {
-                counter = counter++;
 
+            //this loop should be int j = i; no need to run loops over and over
+            //Once I figure out how to print each result once
+
+            for (int j = 0; j < inputArray.length; j++) {
+                if (inputArray[i].equals(inputArray[j])) {
+                    counter++;
+                    tempArray[i] = inputArray[i];
+                }
             }
-            System.out.println("\"" + tempString + "\" occurs " + counter);
-
-
+            System.out.println("\"" + tempArray[i] + "\" occurs " + counter);
         }
-
+        System.out.println(Arrays.toString(tempArray));
     }
 }
